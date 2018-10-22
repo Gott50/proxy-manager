2190dffd66ed4eea2dfd84e29053131dc5863671
Gott50 <xxGott50xx@gmail.com>
2018-10-22 21:27:01 +0200

    add user_proxy_dic

--- src/AWSProxy.py
+++ src/AWSProxy.py
@@ -1,4 +1,15 @@
 
 class AWSProxy:
-    def get(self):
+    def __init__(self):
+        self.user_proxy_dic = {}
+
+    def get(self, user):
+        if user in self.user_proxy_dic:
+            return self.user_proxy_dic[user]
+
+        proxy = self.create_proxy()
+        self.user_proxy_dic[user] = proxy
+        return proxy
+
+    def create_proxy(self):
         return "1.2.3.4:8888"
