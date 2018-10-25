50c47e66ee8d3c5322e6ae98e504e563808f6e72
Gott50 <xxGott50xx@gmail.com>
2018-10-25 09:06:08 +0200

    implement create_new_proxy()

--- src/AWSProxy.py
+++ src/AWSProxy.py
@@ -1,7 +1,12 @@
+import boto3
+from time import sleep
+
+
 class AWSProxy:
     def __init__(self):
         self.user_proxy_dic = {}
         self.stop_proxies()
+        self.ec2 = boto3.resource('ec2')
 
     def get(self, user):
         if user in self.user_proxy_dic:
@@ -18,8 +23,18 @@ class AWSProxy:
         else:
             return self.create_new_proxy()
 
-    def create_new_proxy(self):  # TODO implement
-        return "1.2.3.4:8888"
+    def create_new_proxy(self):
+        instance = self.ec2.create_instances(
+            ImageId='ami-02ae436ce7c43df2b', InstanceType='t2.micro',
+            KeyName='proxy', SecurityGroups=['proxy'],
+            MaxCount=1, MinCount=1
+        )[0]
+
+        while not instance.public_ip_address:
+            sleep(1)
+            instance = self.ec2.Instance(instance.id)
+
+        return '%s:%s' % (instance.public_ip_address, 8888)
 
     def stop_proxies(self):  # TODO implement
         pass
