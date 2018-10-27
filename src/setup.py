76a2d5bfdb702277f3f02db1f4d8f918871cf39e
Gott50 <xxGott50xx@gmail.com>
2018-10-27 09:26:25 +0200

    create Table User in data.db

--- src/setup.py
+++ src/setup.py
@@ -1,6 +1,17 @@
 import logging
+import sqlite3
 
 from AWSProxy import AWSProxy
+
+conn = sqlite3.connect('data.db')
+c = conn.cursor()
+c.execute('''DROP TABLE IF EXISTS user''')
+# Create table
+c.execute('''CREATE TABLE User
+             (name text, proxy text, instance text)''')
+# Save (commit) the changes
+conn.commit()
+conn.close()
 
 aws_proxy = AWSProxy(logger=logging)
 aws_proxy.stop_proxies()
