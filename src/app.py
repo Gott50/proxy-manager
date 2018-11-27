f937ab355f6cb95d0e062ca1f59b6136dc65ef12
Gott50 <xxGott50xx@gmail.com>
2018-11-27 21:55:02 +0100

    use /<user> and Logger with Warning

--- src/app.py
+++ src/app.py
@@ -12,11 +12,10 @@ db = SQLAlchemy(app)
 aws_proxy = AWSProxy(logger=app.logger)
 
 
-@app.route('/', methods=['GET'])
-def get_root():
+@app.route('/<user>', methods=['GET'])
+def get_root(user):
     try:
-        user = request.args.get("user")
-        app.logger.info('create Proxy for: %s' % user)
+        app.logger.warning('create Proxy for: %s' % user)
         proxy = aws_proxy.get(user)
         app.logger.info('return %s, 200' % proxy)
         return proxy, 200
@@ -29,9 +28,9 @@ def get_root():
 def get_stop():
     try:
         user = request.args.get("user")
-        app.logger.info('stop Proxy for: %s' % user)
+        app.logger.warning('stop Proxy for: %s' % user)
         response = aws_proxy.stop(user)
-        app.logger.info('return %s, 200' % response)
+        app.logger.warning('return %s, 200' % response)
         return response, 200
     except Exception as exc:
         # 500 Internal Server Error
